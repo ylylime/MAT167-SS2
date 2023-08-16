8722a81f76330e39f6007411f91de0f8645e6799
Emily Ng <96224511+ylylime@users.noreply.github.com>
2023-08-16 15:08:31 -0700

    Add files via upload

--- MAT167/q2/CGS.py
+++ MAT167/q2/CGS.py
@@ -1,34 +1,32 @@
 import numpy
 
-def gs_cofficient(v1, v2):
-    return numpy.dot(v2, v1) / numpy.dot(v1, v1)
-
-def multiply(cofficient, v):
-    return map((lambda x : x * cofficient), v)
-
 def proj(v1, v2):
-    return multiply(gs_cofficient(v1, v2) , v1)
+    return (numpy.dot(v2, v1) / numpy.dot(v1, v1)) * v1
 
-def gs(X):
+def cgs(X):
     Y = []
-    res = []
+    res = X[i]
     for i in range(len(X)):
         temp_vec = X[i]
         for inY in Y :
+            # proj_vec = (numpy.dot(X[i], inY) / numpy.dot(inY, inY)) * X[i]
             proj_vec = proj(inY, X[i])
-            temp_vec = map(lambda x, y : x - y, temp_vec, proj_vec)
-        Y.append(temp_vec)
+            Y.append(proj_vec)
+
+        for inW in range(0, len(Y)-1):
+            res -= Y[inW+1]
+        return res
+        print(normalize(res))
     return Y
-    # normalize(Y)
+
+def normalize(i):
+    normalized_vector = i / numpy.linalg.norm(i)
+    return normalized_vector
 
 
-def normalize(Y):
-    for i in Y:
-        normalized_vector = i / numpy.linalg.norm(i)
-    return Y
 
 # test = numpy.array([[3.0, 1.0], [2.0, 2.0]])
-test2 = numpy.array([[1.0, 1.0, 0.0], [1.0, 3.0, 1.0], [2.0, -1.0, 1.0]])
+test2 = numpy.array([[1.0, -1.0, 1.0], [1.0, 0.0, 1.0], [1.0, 1.0, 2.0]])
 
 # print numpy.array(gs(test))
-print numpy.array(gs(test2))
+print numpy.array(cgs(test2))
--- MAT167/q2/MGS.py
+++ MAT167/q2/MGS.py
@@ -1,20 +1,45 @@
 import numpy
 
+def proj(v1, v2):
+    return (numpy.dot(v2, v1) / numpy.dot(v1, v1)) * v1
 
-def modified_gs(A:numpy.ndarray)->numpy.ndarray:
-    num_vecs = A.shape[0]
-    num_dims = A.shape[1]
+def mgs(X):
+    Y = []
+    for i in range(len(X)):
+        temp_vec = X[i]
+        for inY in Y :
+            proj_vec = proj(inY, X[i])
+            temp_vec -= proj_vec
+            # temp_vec = normalize(temp_vec)
+            print(normalize(temp_vec))
+        Y.append(temp_vec)
+    return Y
+
+def normalize(i):
+    normalized_vector = i / numpy.linalg.norm(i)
+    return normalized_vector
+
+# test = numpy.array([[3.0, 1.0], [2.0, 2.0]])
+test2 = numpy.array([[1.0, -1.0, 1.0], [1.0, 0.0, 1.0], [1.0, 1.0, 2.0]])
+
+# print numpy.array(gs(test))
+print numpy.array(mgs(test2))
+
+
+
+# def modified_gs(A:numpy.ndarray)->numpy.ndarray:
+#     num_vecs = A.shape[0]
+#     num_dims = A.shape[1]
     
-    L = numpy.zeros(num_vecs)
-    for i in range(num_vecs):
-        L[i] = numpy.sqrt(A[i].T@A[i])
+#     L = numpy.zeros(num_vecs)
+#     for i in range(num_vecs):
+#         L[i] = numpy.sqrt(A[i].T@A[i])
     
     
-    V = A.copy() / L
-    B = V.copy()   
-    for j in range(0, num_vecs):
-        B[j] = V[j]/numpy.sqrt(V[j].T@V[j])
-        for k in range(j, num_vecs):
-            V[k] = V[k] - (B[j].T@V[k])*B[j]    
-    return B
-    
+#     V = A.copy() / L
+#     B = V.copy()   
+#     for j in range(0, num_vecs):
+#         B[j] = V[j]/numpy.sqrt(V[j].T@V[j])
+#         for k in range(j, num_vecs):
+#             V[k] = V[k] - (B[j].T@V[k])*B[j]    
+#     return B
